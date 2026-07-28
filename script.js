fc5a10ba97a72c3cc4836d841cade3d883894768
251020901866 <luuduyquang2000@gmail.com>
2026-07-28 21:47:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function changeImage(img){
+function changeImage(img) {
+
+    console.log("Đổi ảnh");
 
     document.getElementById("mainImage").src = img.src;
-
     document.getElementById("caption").innerHTML = img.alt;
+}
 
+function pageLoaded() {
+
+    console.log("Trang đã tải");
+
+    let images = document.querySelectorAll(".thumbnails img");
+
+    for (let i = 0; i < images.length; i++) {
+
+        console.log("Thêm tabindex cho ảnh " + (i + 1));
+
+        images[i].setAttribute("tabindex", "0");
+    }
 }
